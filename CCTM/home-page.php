e6f02e50ba13b9f6719084872ed36b8643624639
PLURninja <dr.schaphorst@outlook.com>
2017-01-15 16:43:44 -0500

    Navbar edits

--- CCTM/home-page.php
+++ CCTM/home-page.php
@@ -24,37 +24,33 @@
 
 <!-- Navbar will come here -->
 <nav class="navbar navbar-default" role="navigation">
-	<div class="container-fluid">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="#">IPFW Chess Club</a>
-		</div>
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">IPFW Chess Club</a>
+        </div>
 
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			<ul class="nav navbar-nav">
-				<li class="active"><a href="#">Home</a></li>
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Account<b class="caret"></b></a>
-					<ul class="dropdown-menu">
-						<li><a href="#">Login</a></li>
-						<li><a href="#">Register</a></li>
-					</ul>
-				</li>
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Tournament<b class="caret"></b></a>
-					<ul class="dropdown-menu">
-						<li><a href="#">Schedule</a></li>
-						<li><a href="#">Rankings</a></li>
-					</ul>
-				</li>
-			</ul>
-		</div>
-	</div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="home-page.php">Home</a></li>
+                <li><a href="schedule.php">Schedule</a></li>
+                <li><a href="#">Rankings</a></li>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Account<b class="caret"></b></a>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Account<b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Profile</a></li>
+                        <li><a href="user-management.php">User Management</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
 </nav>
 <!-- end navbar -->
 
