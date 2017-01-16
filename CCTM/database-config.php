33f30327202d00f1328d5f8a9f69b14b0418755e
tyler <potttl02@students.ipfw.edu>
2017-01-15 19:23:37 -0500

    Encoding issues

--- CCTM/database-config.php
+++ CCTM/database-config.php
@@ -1,10 +1,10 @@
 <?php
 
 // define database related variables
-$database = 'ChessClub';
-$host = 'localhost';
-$user = 'root';
-$pass = 'root';
+$database = 'ChessClub';
+$host = 'localhost';
+$user = 'root';
+$pass = 'root';
 
 // try to connect to database
 $dbh = new PDO("mysql:dbname={$database};host={$host};port={3306}", $user, $pass);
