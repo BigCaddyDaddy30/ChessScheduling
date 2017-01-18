6bfa18c1aa7445f54a1e98d6e70d50b6912be03e
Mason Snider <masonsnider30@gmail.com>
2017-01-17 22:09:16 -0500

    Started the export data file

--- CCTM/export-data.php
+++ CCTM/export-data.php
@@ -15,4 +15,24 @@ function array2csv(array & $array)
     return ob_get_clean();
 }
 
+function download_send_headers($filename) {
+    // disable caching
+    $now = gmdate("D, d M Y H:i:s");
+    header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+    header("Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate");
+    header("Last-Modified: {$now} GMT");
+
+    // force download  
+    header("Content-Type: application/force-download");
+    header("Content-Type: application/octet-stream");
+    header("Content-Type: application/download");
+
+    // disposition / encoding on response body
+    header("Content-Disposition: attachment;filename={$filename}");
+    header("Content-Transfer-Encoding: binary");
+}
+
+download_send_headers("data_export_" . date("Y-m-d") . ".csv");
+echo array2csv($array);
+die();
 ?>
